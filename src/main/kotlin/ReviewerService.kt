dfc5a3eafb1e2b72f865a4065ff3a9af1061a39e
quii <chris.james@springernature.com>
2024-02-09 14:39:58 +0000

    refactoring

--- src/main/kotlin/ReviewerService.kt
+++ src/main/kotlin/ReviewerService.kt
@@ -3,3 +3,37 @@ data class Submission(val id: String)
 data class Reviewer(val email: String)
 data class Report(val submission: Submission, val reviewer: Reviewer, val content: String)
 
+class ReviewerService{
+    private val submissionsUnderReview: MutableMap<Submission, List<Reviewer>> = mutableMapOf()
+    private val reports: MutableList<Report> = mutableListOf()
+
+    fun review(submission: Submission, shortList: List<Reviewer>) {
+        submissionsUnderReview[submission] = shortList
+    }
+
+    fun saveReport(report: Report) {
+        if(!getShortList(report.submission).contains(report.reviewer)) {
+            throw UnauthorizedReportException(report.reviewer, report.submission)
+        }
+
+        reports.add(report)
+    }
+
+    fun unInvite(submission: Submission, reviewer: Reviewer) {
+        val shortList: MutableList<Reviewer> = getShortList(submission).toMutableList()
+        shortList.remove(reviewer)
+        submissionsUnderReview[submission] = shortList
+    }
+
+    fun getReports(submission: Submission): List<Report> {
+        return reports.filter { it.submission==submission }
+    }
+
+    fun getShortList(submission: Submission): List<Reviewer> {
+        return submissionsUnderReview[submission] ?: throw SubmissionDoesntExistException(submission)
+    }
+
+    fun closeReview(submission: Submission) {
+        submissionsUnderReview.remove(submission)
+    }
+}
--- src/test/kotlin/ReviewerServiceTest.kt
+++ src/test/kotlin/ReviewerServiceTest.kt
@@ -5,48 +5,13 @@ import org.junit.jupiter.api.Test
 import strikt.api.*
 import strikt.assertions.*
 
-class ReviewerService{
-    private val submissionsUnderReview: MutableMap<Submission, List<Reviewer>> = mutableMapOf()
-    private val reports: MutableList<Report> = mutableListOf()
-
-    fun review(submission: Submission, shortList: List<Reviewer>) {
-        submissionsUnderReview[submission] = shortList
-    }
-
-    fun saveReport(report: Report) {
-        if(!getShortList(report.submission).contains(report.reviewer)) {
-            throw UnauthorizedReportException(report.reviewer, report.submission)
-        }
-
-        reports.add(report)
-    }
-
-    fun unInvite(submission: Submission, reviewer: Reviewer) {
-        val shortList: MutableList<Reviewer> = getShortList(submission).toMutableList()
-        shortList.remove(reviewer)
-        submissionsUnderReview[submission] = shortList
-    }
-
-    fun getReports(submission: Submission): List<Report> {
-        return reports.filter { it.submission==submission }
-    }
-
-    fun getShortList(submission: Submission): List<Reviewer> {
-        return submissionsUnderReview[submission] ?: throw SubmissionDoesntExistException(submission)
-    }
-
-    fun closeReview(submission: Submission) {
-        submissionsUnderReview.remove(submission)
-    }
-}
-
 class ReviewerServiceTest {
     private val sut = ReviewerService()
+    private val aSubmission = Submission("some id")
 
     @Test
     fun `an editor can give a shortlist for a submission to the reviewer service to get some reports`() {
         val shortList = listOf(chrisTheReviewer, edTheReviewer, bernardoTheReviewer)
-        val aSubmission = Submission("some id")
 
         sut.review(aSubmission, shortList)
 
@@ -60,7 +25,6 @@ class ReviewerServiceTest {
     @Test
     fun `reviewers who were not shortlisted cannot review`() {
         val shortList = listOf(edTheReviewer, bernardoTheReviewer)
-        val aSubmission = Submission("some id")
 
         sut.review(aSubmission, shortList)
 
@@ -77,7 +41,6 @@ class ReviewerServiceTest {
     @Test
     fun `once a decision is made, review is closed, and reports will not be accepted`() {
         val shortList = listOf(chrisTheReviewer, edTheReviewer, bernardoTheReviewer)
-        val aSubmission = Submission("some id")
         val chrisReport = Report(aSubmission, chrisTheReviewer, "This paper sucks")
         val edReport = Report(aSubmission, edTheReviewer, "This paper is amazing")
 
@@ -94,7 +57,6 @@ class ReviewerServiceTest {
     @Test
     fun `can remove from the shortlist, which would prevent reports from that reviewer`() {
         val shortList = listOf(chrisTheReviewer, edTheReviewer, bernardoTheReviewer)
-        val aSubmission = Submission("some id")
 
         sut.review(aSubmission, shortList)
         sut.unInvite(aSubmission, chrisTheReviewer)
@@ -106,13 +68,17 @@ class ReviewerServiceTest {
     @Test
     fun `during review, i can see who is invited`() {
         val shortList = listOf(chrisTheReviewer, edTheReviewer, bernardoTheReviewer)
-        val aSubmission = Submission("some id")
         sut.review(aSubmission, shortList)
         expectThat(sut.getShortList(aSubmission)).contains(shortList)
     }
 
     @Test
     fun `once a decision is made, reviewers who have not sent a report, will be notified`() {
+
+    }
+
+    @Test
+    fun `reviewers are notified when they are shortlisted to review a submission`() {
 
     }
 
